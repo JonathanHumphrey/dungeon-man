94d47d0dd2fe0db427748382fd68d1803d87d4db
jonathanhumphrey <jlh473@txstate.edu>
2021-02-09 12:02:00 -0600

    added styling and sound for inventory

--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useSound from 'use-sound'
+
+import zipper from '../Sounds/zipper.mp3'
 
 export default function Stats({player, inventory}) {
+    const [show, setShow] = useState(false)
+
+    
+    const [play] = useSound(zipper, {
+        volume: 0.15,
+    })
+    
+    const toggleShow = () => {
+        setShow(!show)
+        console.log(zipper)
+        play()
+    }
+
+    const delItem = (key) => {
+        console.log(inventory.items)
+        //delete inventory.items
+    }
     return (
         <div className='statsWindow'>
             <h4>Player Stats</h4>
@@ -8,14 +28,23 @@ export default function Stats({player, inventory}) {
             <p>Class: {player.class}</p>
 
             <div className='inventoryWindow'>
-                <h4>Inventory</h4>
-                <p>Gold: {inventory.gold}</p>
-                <hr />
-                {inventory.items.map((item, index) => (
-                    <ul>
-                        <li key={index}>{item}</li>
-                    </ul>
-                ))}
+                <div className='actualInventory'>
+                    <h4 className='inv' onClick={toggleShow}>Inventory</h4>
+                    {show ? 
+                        <div className=''>
+                            <hr />
+                            <p>Gold: {inventory.gold}</p>
+                            {inventory.items.map((item, index) => (
+                                <ul>
+                                    <li key={index}>{item}
+                                        <button className='del' onClick={delItem} key={index}>x</button>
+                                    </li>
+                                </ul>
+                            ))}
+                        </div>
+                    : null
+                    }
+                </div>
             </div>
         </div>
 
